fd8ca9dff6d67681d0c98baaff321369cc10f282
InterLink Intern <intern@intern-pc.interlink.office>
2019-03-15 12:49:59 +0200

    Refractor code

--- src/app/components/preview-page/preview-page.component.ts
+++ src/app/components/preview-page/preview-page.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TodolistsService } from '../../shared/services/todolists.service';
-
 import { Router } from '@angular/router';
-import { resolve, reject } from 'q';
-
 
 @Component({
   selector: 'app-preview-page',
@@ -15,51 +12,42 @@ export class PreviewPageComponent implements OnInit {
   constructor(
     private todolistsService: TodolistsService,
     private route: Router
-  ) {}
+  ) { }
 
   lists;
   tasks = [];
-  
+
   ngOnInit() {
     this.getListsAndTasks();
   }
-  
+
   getListsAndTasks() {
-      this.todolistsService
+    this.todolistsService
       .getLists()
       .subscribe(res => {
         this.lists = res;
         this.lists.map(list => {
           this.todolistsService
-          .getTasksFromSelectedList(list.id)
-          .subscribe(res => { 
-            list.listTasks = this.sortListTasksForThreeUndone(res);
-            // this.filterListWithNoTasks();
-          });
+            .getTasksFromSelectedList(list.id)
+            .subscribe(res => {
+              list.listTasks = this.sortListTasksForThreeUndone(res);
+            });
         })
-        .forEach(item => console.log('item', item));
-        console.log(this.lists);
       });
-    
+
   }
-  
+
   sortListTasksForThreeUndone(tasks) {
-    const listId = tasks[0].id;
     tasks = tasks.filter(task => !task.done);
     if (tasks.length > 5) {
       tasks[5].name = '...';
       tasks.length = 6;
-      // return tasks;
     } else if (tasks.length === 0) {
       tasks = [{
         name: 'Everything is done.'
       }];
     }
-    return tasks; 
-  }
-
-  filterListWithNoTasks() {
-    this.lists.forEach(list => console.log('list', list.listTasks));
+    return tasks;
   }
 
   navigateToTodolist(listId) {
