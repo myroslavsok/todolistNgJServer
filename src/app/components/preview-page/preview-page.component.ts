f4c21f94ae71d1e689b1c971d43813e7402d1402
InterLink Intern <intern@intern-pc.interlink.office>
2019-03-15 12:45:21 +0200

    Set 'everything is done' value for lists without tasks

--- src/app/components/preview-page/preview-page.component.ts
+++ src/app/components/preview-page/preview-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TodolistsService } from '../../shared/services/todolists.service';
 
 import { Router } from '@angular/router';
+import { resolve, reject } from 'q';
 
 
 @Component({
@@ -17,32 +18,48 @@ export class PreviewPageComponent implements OnInit {
   ) {}
 
   lists;
-  tasks;
+  tasks = [];
   
   ngOnInit() {
     this.getListsAndTasks();
   }
   
   getListsAndTasks() {
-    this.todolistsService
+      this.todolistsService
       .getLists()
       .subscribe(res => {
         this.lists = res;
         this.lists.map(list => {
           this.todolistsService
-            .getTasksFromSelectedList(list.id)
-            .subscribe(res => list.listTasks = this.sortListTasksForThreeUndone(res));
-        });
+          .getTasksFromSelectedList(list.id)
+          .subscribe(res => { 
+            list.listTasks = this.sortListTasksForThreeUndone(res);
+            // this.filterListWithNoTasks();
+          });
+        })
+        .forEach(item => console.log('item', item));
+        console.log(this.lists);
       });
+    
   }
-
+  
   sortListTasksForThreeUndone(tasks) {
-    tasks = tasks.filter(task => (task.done));
+    const listId = tasks[0].id;
+    tasks = tasks.filter(task => !task.done);
     if (tasks.length > 5) {
       tasks[5].name = '...';
       tasks.length = 6;
+      // return tasks;
+    } else if (tasks.length === 0) {
+      tasks = [{
+        name: 'Everything is done.'
+      }];
     }
-    return tasks;
+    return tasks; 
+  }
+
+  filterListWithNoTasks() {
+    this.lists.forEach(list => console.log('list', list.listTasks));
   }
 
   navigateToTodolist(listId) {
